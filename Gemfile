f956957ff0add1ca0058f3734efe7593d6ffd244
Alexis Creuzot <alexis.creuzot@gmail.com>
2023-05-18 18:49:53 -0600

    delete 30 files and update 169 files

--- Gemfile
+++ Gemfile
@@ -1,6 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'jekyll', '~> 3.8'
-gem 'jekyll-assets', '~> 2.3.2'
-gem "uglifier"
-gem "sass"
+gem 'jekyll'
---
+++ docs/scripts/vendor.js
@@ -0,0 +1,6 @@
+//= require vendor/jquery/jquery-3.0.0-alpha1.js
+//= require vendor/jquery/jquery.backstretch.js
+//= require vendor/jquery/jquery.particleground.js
+//= require vendor/jquery/jquery.waitforimages.min.js
+//= require vendor/fastclick.js
+//= require vendor/milkyway.js
